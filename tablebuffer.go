e11c9adb66363bbd32d44c5cdf7d843782a2c9c5
Daniel Theophanes <kardianos@gmail.com>
2012-02-25 21:51:24 -0800

    go fmt

--- tablebuffer.go
+++ tablebuffer.go
@@ -1,7 +1,6 @@
 // Provides a simple buffer for results from database/sql.
 package tablebuffer
 
-
 import (
 	"database/sql"
 	"errors"
@@ -13,8 +12,8 @@ type RowBuffer []interface{}
 // Table buffer provides a buffer for you table.  ColumnNameMap is the
 // "inverse" of ColumnNames.
 type TableBuffer struct {
-	Rows []RowBuffer
-	ColumnNames []string
+	Rows          []RowBuffer
+	ColumnNames   []string
 	ColumnNameMap map[string]int
 }
 
@@ -23,44 +22,44 @@ type TableBuffer struct {
 func FillRowBuffer(rows *sql.Rows) (table *TableBuffer, err error) {
 	var out []interface{}
 	var dest []interface{}
-	
+
 	table = &TableBuffer{
 		Rows: make([]RowBuffer, 0),
 	}
 	first := true
-	colCount := 0;
+	colCount := 0
 	for rows.Next() {
 		// Some initialization depends on knowing the column names
 		// which isn't available until the first row is fetched.
 		if first {
 			first = false
-			
+
 			// Get the column names.
 			table.ColumnNames, err = rows.Columns()
 			if err != nil {
 				return
 			}
 			colCount = len(table.ColumnNames)
-			
+
 			// Create an easy lookup that should be more efficent then
 			// always looping to lookup an index from a column name.
 			table.ColumnNameMap = make(map[string]int, colCount)
 			for i, n := range table.ColumnNames {
 				table.ColumnNameMap[n] = i
 			}
-			
+
 			// Create a sized pointer slice.
 			dest = make([]interface{}, colCount)
 		}
 		// Create a new data slice that will be appended on to the table.
 		out = make([]interface{}, colCount)
-		
+
 		// Scanning requires having a pointer to the data slice,
 		// so first make a pointer slice to each element of the data slice.
 		for i, _ := range dest {
-		   dest[i] = &out[i]
+			dest[i] = &out[i]
 		}
-		
+
 		// Then scan into the pointer slice.
 		err = rows.Scan(dest...)
 		if err != nil {
@@ -94,4 +93,3 @@ func (t *TableBuffer) GetPanic(rowIndex int, columnName string) interface{} {
 	}
 	return t.Rows[rowIndex][i]
 }
-
